272eefc7fe1dadf849004ac24d7ca9bc776fd9ed
Luiz Henrique de Oliveira <luizhsimionato@gmail.com>
2023-04-05 16:14:16 -0300

    equipe

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import Equipe from "./components/equipe";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Componentes e Props</h1>
+      <Equipe nome='luiz' cargo= ' profissional de educação fisica' />
+      <hr/>
+      <Equipe nome='Daniela' cargo= ' Comerciante' />
+      <hr/>
+      <Equipe nome='Juliano' cargo= ' Contador' />
+      <hr/>
     </div>
   );
 }
---
+++ src/components/equipe/index.js
@@ -0,0 +1,12 @@
+
+
+function Equipe(props){
+    return(
+        <div>
+            <h2>Equipe:</h2><br />
+            <h3>Nome:-{props.nome} Cargo:-{props.cargo}</h3>
+        </div>
+    )
+}
+
+export default Equipe
